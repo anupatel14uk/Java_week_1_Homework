735366611a0b66417085230cee56bfbe0a7a755c
anupatel14uk <anupatel14uk@gmail.com>
2023-05-17 20:59:17 +0100

    java week 1 homework aruna

---
+++ src/java_week_1_writing_hw_aruna/Program1_1_BioData.java
@@ -0,0 +1,23 @@
+package java_week_1_writing_hw_aruna;
+/**
+ * 1. Write a program that prints your BIO Data.
+ */
+public class Program1_1_BioData {
+            public static void main(String[] argh) {
+            System.out.println("              BIO DATA                ");
+            System.out.println("Name              : Janvi Patel");
+            System.out.println("Mobile            : 07912 345678");
+            System.out.println("Email             : janviPatel@gmail.com");
+            System.out.println("Gender            : Female");
+            System.out.println("Date of Birth     : 01.05.2000");
+            System.out.println("Marital Status    : Single");
+            System.out.println("Languages Known   : English,Gujarati,Hindi");
+            System.out.println("Qualification     : Software Tester");
+            System.out.println("Experience        : Agile Tester with 5 years of experience");
+            System.out.println("Address           : 123 Abc Street, London AB1 2CD");
+            System.out.println("Place             : London");
+            System.out.println("Date              : 02.10.2021");
+            System.out.println("Signature         : Jpatel");
+        }
+    }
+
